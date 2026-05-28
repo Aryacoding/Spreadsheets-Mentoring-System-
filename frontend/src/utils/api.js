16024a81259b94c57d21dfc5822fb605eb6bee3a
Aryacoding <ariabima729@gmail.com>
2026-05-28 21:50:15 +0700

    Update 6.2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-// Ubah alamat ini dengan URL Backend Vercel milikmu nanti
-const BASE_URL = "https://bastp-backend-api.vercel.app/"; 
+
+const BASE_URL = "https://bastp-backend-api.vercel.app"; 
 
 export default BASE_URL;
